234e2401fdc0da2d0de5dd9a978683d63c13260d
lazyykurt <lazyyykurt@gmail.com>
2024-07-17 21:39:33 +0800

    使用react router loader，完成 single page的数据加载

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -43,24 +43,7 @@ export const getPost = async (req, res) => {
       },
     });
 
-    const token = req.cookies?.token;
-
-    if (token) {
-      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-        if (!err) {
-          const saved = await prisma.savedPost.findUnique({
-            where: {
-              userId_postId: {
-                postId: id,
-                userId: payload.id,
-              },
-            },
-          });
-          res.status(200).json({ ...post, isSaved: saved ? true : false });
-        }
-      });
-    }
-    res.status(200).json({ ...post, isSaved: false });
+    res.status(200).json({ ...post });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get post" });
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 import NewPostPage from "./routes/newPostPage/newPostPage";
+import { singlePageLoader, listPageLoader } from "./lib/loaders";
 
 function App() {
   const router = createBrowserRouter([
@@ -25,10 +26,12 @@ function App() {
         {
           path: "/list",
           element: <ListPage />,
+          loader: listPageLoader,
         },
         {
           path: "/:id",
           element: <SinglePage />,
+          loader: singlePageLoader,
         },
         {
           path: "/login",
